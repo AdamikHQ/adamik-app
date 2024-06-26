c0d33962a2dbf77caa859261ad06fb0bbc51f578
fabrice-adamik <159912605+fabrice-adamik@users.noreply.github.com>
2024-06-26 13:17:05 +0200

    fixed vercel error

--- src/app/portfolio/AssetRow.tsx
+++ src/app/portfolio/AssetRow.tsx
@@ -1,11 +1,6 @@
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { TableCell, TableRow } from "~/components/ui/table";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "~/components/ui/tooltip";
+import { Tooltip, TooltipProvider, TooltipTrigger, TooltipContent } from "~/components/ui/tooltip";
 import { formatAmountUSD } from "~/utils/helper";
 import { Asset } from "~/utils/types";
 
@@ -18,33 +13,24 @@ export const AssetRow: React.FC<{ asset: Asset }> = ({ asset }) => {
             <div>
               {asset?.logo && (
                 <div className="relative">
-                  <Tooltip>
+                  <Tooltip text={asset.name}>
                     <TooltipTrigger>
                       <Avatar className="w-[38px] h-[38px]">
                         <AvatarImage src={asset?.logo} alt={asset.name} />
                         <AvatarFallback>{asset.name}</AvatarFallback>
                       </Avatar>
                     </TooltipTrigger>
-                    <TooltipContent>
-                      <p>{asset.name}</p>
-                    </TooltipContent>
                   </Tooltip>
                   {asset.mainChainLogo && (
-                    <Tooltip>
+                    <Tooltip text={asset.chainId}>
                       <TooltipTrigger>
                         <div className="absolute w-5 h-5 text-xs font-bold text-primary bg-primary-foreground border-2 rounded-full -top-2 end-2">
                           <Avatar className="h-4 w-4">
-                            <AvatarImage
-                              src={asset.mainChainLogo}
-                              alt={asset.chainId}
-                            />
+                            <AvatarImage src={asset.mainChainLogo} alt={asset.chainId} />
                             <AvatarFallback>{asset.chainId}</AvatarFallback>
                           </Avatar>
                         </div>
                       </TooltipTrigger>
-                      <TooltipContent>
-                        <p>{asset.chainId}</p>
-                      </TooltipContent>
                     </Tooltip>
                   )}
                 </div>
