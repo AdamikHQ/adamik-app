11614b9a61ef4428465cdd3bac16ede782a76d8f
fabrice-adamik <159912605+fabrice-adamik@users.noreply.github.com>
2024-07-04 21:33:00 +0200

    new banner design

--- src/components/layout/ShowroomBanner.tsx
+++ src/components/layout/ShowroomBanner.tsx
@@ -1,8 +1,8 @@
 export const ShowroomBanner = () => {
   return (
-    <div className="w-full bg-muted-foreground/50 text-center text-2xl py-2 rounded-md">
-      You are on the demo version of the Adamik App. To experience all features,
-      please connect your wallet
+    <div className="w-full bg-gray-800/70 text-center text-base py-2 rounded-full border border-gray-600 text-white flex items-center justify-center gap-2">
+      You are using the demo version of Adamik App. Connect your wallet to
+      access all features.
     </div>
   );
 };
