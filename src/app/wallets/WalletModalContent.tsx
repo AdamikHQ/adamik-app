eddb690b2ea10df766935b16dfdd028b9d6a59f5
hakim-adamik <hakim@adamik.io>
2024-07-07 23:59:39 +0200

    Support Pera Wallet

--- src/app/wallets/WalletModalContent.tsx
+++ src/app/wallets/WalletModalContent.tsx
@@ -3,6 +3,7 @@
 import { useWallet } from "~/hooks/useWallet";
 import { KeplrConnect } from "./KeplrConnect";
 import { MetamaskConnect } from "./MetamaskConnect";
+import { PeraConnect } from "./PeraConnect";
 import { Address } from "./types";
 
 export const WalletModalContent = () => {
@@ -24,6 +25,9 @@ export const WalletModalContent = () => {
       return [...acc, ...familyAddresses];
     }, []);
 
+    // FIXME DEBUG TBR
+    console.log("XXX - addresses:", addresses);
+
     setShowroom(false);
     addAddresses(addresses);
   };
@@ -32,6 +36,7 @@ export const WalletModalContent = () => {
     <div className="flex flex-row gap-4">
       <MetamaskConnect setWalletAddresses={setWalletAddresses} />
       <KeplrConnect setWalletAddresses={setWalletAddresses} />
+      <PeraConnect setWalletAddresses={setWalletAddresses} />
     </div>
   );
 };
