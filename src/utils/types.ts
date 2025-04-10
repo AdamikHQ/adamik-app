9de124f745407ddaad6e070e33eeb1da7d3e49d1
fabrice-adamik <159912605+fabrice-adamik@users.noreply.github.com>
2025-04-10 22:31:13 +0200

    re-org types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -257,3 +257,20 @@ export interface TransactionFees {
   amount: string;
   ticker?: string;
 }
+
+export enum AdamikCurve {
+  SECP256K1 = "secp256k1",
+  ED25519 = "ed25519",
+}
+
+export enum AdamikHashFunction {
+  SHA256 = "sha256",
+  KECCAK256 = "keccak256",
+}
+
+export interface AdamikSignerSpec {
+  curve: AdamikCurve;
+  hashFunction: AdamikHashFunction;
+  coinType: string;
+  signatureFormat: string;
+}
